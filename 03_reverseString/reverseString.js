97334464570e76a20ef31f25008e49673864173f
Yuval <yuvaln09@gmail.com>
2025-01-17 17:51:16 +0200

    answers update

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let word = string.split(''); // Split the word into array of characters
+    let reverseWord = word.reverse(); // Reverse the array
+    let joinWord = reverseWord.join(''); // Join the array into a new string
+    return joinWord;
 };
 
 // Do not edit below this line
